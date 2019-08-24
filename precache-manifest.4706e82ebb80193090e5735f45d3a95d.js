639e3a44cee22b2096886d73184883c77ac2ad3a
Sangseok Doe <sangseoksoftware@outlook.com>
2019-08-24 00:28:16 -0400

    Updates

---
+++ precache-manifest.4706e82ebb80193090e5735f45d3a95d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9721eb8acafabd1871a905ff0d2bae94",
+    "url": "/antiredux/index.html"
+  },
+  {
+    "revision": "330ed0ebc08e121ff164",
+    "url": "/antiredux/static/js/2.de341e56.chunk.js"
+  },
+  {
+    "revision": "3ceac6dda501f6acd3a9",
+    "url": "/antiredux/static/js/main.eaea9944.chunk.js"
+  },
+  {
+    "revision": "62aac7d50d4e62140440",
+    "url": "/antiredux/static/js/runtime~main.8e16fd43.js"
+  }
+]);
